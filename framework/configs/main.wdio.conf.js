b1f586023c112db752cd1a6489e669e26c921786
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2024-05-18 22:38:32 +0600

    allure report configuration is done

--- framework/configs/main.wdio.conf.js
+++ framework/configs/main.wdio.conf.js
@@ -1,5 +1,11 @@
 import path from 'node:path';
 import fs from 'fs-extra';
+import fs1 from 'fs';
+import Logger from '../utils/Logger.js';
+import allure from "allure-commandline";
+
+const allureDir = path.resolve("./report/allure/");
+/* const allureDir = "./report/allure/"; */
 
 export const downloadDir = path.resolve('./tmp');
 
@@ -14,7 +20,14 @@ export const mainConfig = {
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: allureDir+'/allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+        }]
+    ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
@@ -33,5 +46,39 @@ export const mainConfig = {
             await browser.takeScreenshot();
         }
     },
+
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', allureDir+'/allure-results', '--clean', '-o', allureDir+'/test-reports'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                /* console.log() */
+                Logger.info('Allure report successfully generated');
+                resolve()
+            })
+        })
+    },
+
+    beforeSuite: async function (){
+        try{
+            if(fs.existsSync(allureDir)){
+                fs.rmSync(allureDir, {recursive: true})
+            }
+        }catch(err){
+            Logger.error('file failure');
+            fs.mkdirSync(allureDir, {recursive: true});
+            Logger.info('Allure dir got created');
+        }
+    }
 
 }
